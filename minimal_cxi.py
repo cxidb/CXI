Define relation between physical and logical dimension. Remove non SI units. Add more fields to the classes. Fix the examples according to new dimensions.

minimal_cxi.py
@@ -10,7 +10,7 @@ f = h5py.File(fileName, "w")
 
 # create data
 x = np.arange(-5, 5, 0.1)
-y = np.arange(-5, 5, 0.2)
+y = np.arange(0, 5, 0.1)
 xx, yy = np.meshgrid(x, y)
 sinc = np.sin(xx**2+yy**2)/(xx**2+yy**2)
 

nexus_cxi.py
@@ -15,20 +15,22 @@ f.create_dataset("cxi_version",data=100)
 
 # create data 1
 x = np.arange(-5, 5, 0.1)
-y = np.arange(-5, 5, 0.2)
+y = np.arange(0, 5, 0.1)
 xx, yy = np.meshgrid(x, y)
 sinc1 = np.sin(xx**2+yy**2)/(xx**2+yy**2)
 
 # create data 2
 x = np.arange(-1, 1, 0.02)
-y = np.arange(-1, 1, 0.04)
+y = np.arange(0, 1, 0.02)
 xx, yy = np.meshgrid(x, y)
 sinc2 = np.sin(xx**2+yy**2)/(xx**2+yy**2)
 
 # populate the file with the classes tree    
 entry_1 = f.create_group("entry_1")
-entry_1.create_dataset("experimental_identifier",data="LCLS_2009_Dec11_170451_21963")
-entry_1.create_dataset("start_time",data="2009-12-11T17:04:51-0800")
+entry_1.create_dataset("experimental_identifier",data=
+                       "LCLS_2009_Dec11_170451_21963")
+entry_1.create_dataset("start_time",data=
+                       "2009-12-11T17:04:51-0800")
 entry_1.attrs['NX_class'] = "NXentry"
 
 sample_1 = entry_1.create_group("sample_1")
@@ -40,14 +42,16 @@ instrument_1.create_dataset("name",data="AMO")
 instrument_1.attrs['NX_class'] = "NXinstrument"
 
 source_1 = instrument_1.create_group("source_1")
-energy = source_1.create_dataset("energy",data=1803.4) # in eV
-energy.attrs['units'] = "eV"
-pulse_width = source_1.create_dataset("pulse_width",data=70) # in fs
-pulse_width.attrs['units'] = "fs"
+energy = source_1.create_dataset("energy",data=2.8893e-16) # in J
+energy.attrs['units'] = "J"
+pulse_width = source_1.create_dataset("pulse_width",
+                                      data=70e-15) # in s
+pulse_width.attrs['units'] = "s"
 source_1.attrs['NX_class'] = "NXsource"
 
 detector_1 = instrument_1.create_group("detector_1")
-distance = detector_1.create_dataset("distance",data=0.15) # in meters
+distance = detector_1.create_dataset("distance",
+                                     data=0.15) # in meters
 distance.attrs['units'] = "m"
 data = detector_1.create_dataset("data",data=sinc1)
 data.attrs['signal'] = 1

phased_3d_cxi.py
@@ -10,7 +10,7 @@ f = h5py.File(fileName, "w")
 f.create_dataset("cxi_version",data=100)
 
 # create data
-xx, yy, zz = np.mgrid[-5:5:16j, -5:5:16j, -5:5:16j]
+zz, yy, xx = np.mgrid[-5:5:8j, -5:5:12j, -5:5:16j]
 sinc = np.sin(xx**2+yy**2+zz**2)/(xx**2+yy**2+zz**2) + \
        1j*np.cos(xx**2+yy**2+zz**2)/(xx**2+yy**2+zz**2)
 
@@ -26,7 +26,7 @@ image_1.create_dataset("data_space",data="real")
 image_1.create_dataset("image_size",
                        data=[1.65e-6,1.65e-6,1.65e-6])
 source_1 = image_1.create_group("source_1")
-source_1.create_dataset("energy",data=1803.4) # in eV
+source_1.create_dataset("energy",data=2.8893e-16) # in J
 detector_1 = image_1.create_group("detector_1")
 detector_1.create_dataset("distance",
                           data=0.15) # in meters

typical_raw_cxi.py
@@ -24,22 +24,28 @@ sinc2 = np.sin(xx**2+yy**2)/(xx**2+yy**2)
 
 # populate the file with the classes tree    
 entry_1 = f.create_group("entry_1")
-entry_1.create_dataset("experimental_identifier",data="LCLS_2009_Dec11_170451_21963")
-entry_1.create_dataset("start_time",data="2009-12-11T17:04:51-0800")
+entry_1.create_dataset("experimental_identifier",data=
+                       "LCLS_2009_Dec11_170451_21963")
+entry_1.create_dataset("start_time",data=
+                       "2009-12-11T17:04:51-0800")
 sample_1 = entry_1.create_group("sample_1")
 sample_1.create_dataset("name",data="Mimivirus")
 instrument_1 = entry_1.create_group("instrument_1")
 instrument_1.create_dataset("name",data="AMO")
 source_1 = instrument_1.create_group("source_1")
-source_1.create_dataset("energy",data=1803.4) # in eV
-source_1.create_dataset("pulse_width",data=70) # in fs
+source_1.create_dataset("energy",
+                        data=2.8893e-16) # in J
+source_1.create_dataset("pulse_width",
+                        data=70e-15) # in s
 
 detector_1 = instrument_1.create_group("detector_1")
-detector_1.create_dataset("distance",data=0.15) # in meters
+detector_1.create_dataset("distance",
+                          data=0.15) # in meters
 detector_1.create_dataset("data",data=sinc1)
 
 detector_2 = instrument_1.create_group("detector_2")
-detector_2.create_dataset("distance",data=0.65) # in meters
+detector_2.create_dataset("distance",
+                          data=0.65) # in meters
 detector_2.create_dataset("data",data=sinc2)
 
 data_1 = entry_1.create_group("data_1")